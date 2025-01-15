ffcede9a7d5915929400b33ed98ac44b03185cd1
PrachiKarle <prachikkarle@gmail.com>
2025-01-15 23:13:26 +0530

    Second Commit

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,51 @@
 var express=require('express');
 const router =express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('admin/index.ejs');
+var exe=require('../connection');
+
+router.get('/',async(req,res)=>{
+    var sql=`select* from user`;
+    var d=await exe(sql);
+    res.render('admin/index.ejs',{data:d});
 })
+
+
+
+//collections
+router.get('/collections',async(req,res)=>{
+    var sql=`select* from collections`;
+    const obj={data:await exe(sql)};
+    res.render('admin/collection.ejs',obj);
+})
+
+//insert 
+router.post('/savecoll',async(req,res)=>{
+     const {coll_name,coll_price}=req.body;
+
+     var files=req.files.coll_img;
+     var filename=new Date().getTime()+"_"+files.name;
+     files.mv('public/uploads/'+filename);
+
+     var sql=`insert into collections(Name,Price,Image) values(?,?,?)`;
+     await exe(sql,[coll_name,coll_price,filename]);
+     res.redirect('/admin/collections');
+})
+
+// delete
+router.get('/del_coll/:id',async(req,res)=>{
+    var id=req.params.id;
+    var sql=`delete from collections where Id=?`;
+    await exe(sql,[id]);
+    res.redirect('/admin/collections');
+})
+
+//update
+router.get('/edit_coll/:id',async(req,res)=>{
+    var id=req.params.id;
+    var sql=`select* from collections where Id=?`;
+    var d=await exe(sql,[id]);
+    res.render('admin/editColl.ejs',{data:d[0]});
+})
+
 
 module.exports=router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,111 +1,160 @@
-var express=require('express');
-const router=express.Router();
-var exe=require('../connection');
+var express = require("express");
+const router = express.Router();
+var exe = require("../connection");
+var sendOTP = require("../Email");
 
-// otp
-var sendOTP=require('../Email');
 
-router.get('/',(req,res)=>{
-    var name="";
-    if(req.session.login_id){
-        name=req.session.login_name;
-    }
-    const obj={usernm:name}
-    res.render('user/index.ejs',obj);
+
+
+//home page
+router.get("/", (req, res) => {
+  var name = "";
+  if (req.session.login_id) {
+    name = req.session.login_name;
+  }
+  const obj = { usernm: name };
+  res.render("user/index.ejs", obj);
+});
+
+
+
+
+
+//customer care page
+router.get("/customercare", (req, res) => {
+  var name = "";
+  if (req.session.login_id) {
+    name = req.session.login_name;
+  }
+  const obj = { usernm: name };
+  res.render("user/customer.ejs", obj);
+});
+
+router.post("/savecontact", async (req, res) => {
+  if (req.session.login_id) {
+    const { username, useremail, userno, usermsg } = req.body;
+
+    var sql = `insert into contact(user_id,user_name,user_email,user_no,user_msg) values(?,?,?,?,?)`;
+    var values = [req.session.login_id, username, useremail, userno, usermsg];
+    await exe(sql, values);
+
+    res.redirect("/");
+  } else {
+    res.redirect("/sign");
+  }
+});
+
+
+
+
+//collections
+router.get("/collection", async (req, res) => {
+  var name = "";
+  if (req.session.login_id) {
+    name = req.session.login_name;
+  }
+
+  var sql = `select* from collections`;
+  var d1 = await exe(sql);
+
+  const obj = { usernm: name, data: d1 };
+  res.render("user/collection1.ejs", obj);
+});
+
+// accessories
+router.get('/accessories',async(req,res)=>{
+    var name = "";
+  if (req.session.login_id) {
+    name = req.session.login_name;
+  }
+
+  var sql = `select* from accessories`;
+  var d1 = await exe(sql);
+
+  const obj = { usernm: name, data: d1 };
+  res.render("user/accessories.ejs", obj);
 })
+
+
+
 
 
 
 //login
-router.get('/sign',(req,res)=>{
-    res.render('user/sign.ejs');
-})
+router.get("/sign", (req, res) => {
+  res.render("user/sign.ejs");
+});
 
-router.post('/loginuser',async(req,res)=>{
-    const {useremail,userpass}=req.body;
-    var sql=`select* from user where useremail='${useremail}' AND userpass='${userpass}'`;
-    var data=await exe(sql);
-    
-    if(data.length>0){
+router.post("/loginuser", async (req, res) => {
+  const { useremail, userpass } = req.body;
+  var sql = `select* from user where useremail='${useremail}' AND userpass='${userpass}'`;
+  var data = await exe(sql);
 
-        req.session.user_id=data[0].userid;
-        req.session.name=data[0].username;
+  if (data.length > 0) {
+    req.session.user_id = data[0].userid;
+    req.session.name = data[0].username;
 
-        var otp=Math.trunc(Math.random()*10000);
-        req.session.user_otp=otp;
+    var otp = Math.trunc(Math.random() * 10000);
+    req.session.user_otp = otp;
 
-        sendOTP(useremail,data[0].username,otp);
+    sendOTP(useremail, data[0].username, otp);
 
-        res.redirect('/accept_otp');
-    }
-    else{
-        res.redirect('/loginuser');
-    }
-})
-router.get('/accept_otp',(req,res)=>{
-    if(req.session.user_id){
-       res.render('user/acceptOtp.ejs');
-    }
-    else{
-        res.redirect('/loginuser');
-    }
-})
-router.post('/verifyotp',(req,res)=>{
-    if(req.body.otp==req.session.user_otp){
-
-        req.session.login_id=req.session.user_id;
-        req.session.login_name=req.session.name;
-        res.redirect('/');
-
-    }
-    else{
-        res.redirect('/accept_otp');
-    }
-})
-
-
-
-
+    res.redirect("/accept_otp");
+  } else {
+    res.redirect("/loginuser");
+  }
+});
+router.get("/accept_otp", (req, res) => {
+  if (req.session.user_id) {
+    res.render("user/acceptOtp.ejs");
+  } else {
+    res.redirect("/loginuser");
+  }
+});
+router.post("/verifyotp", (req, res) => {
+  if (req.body.otp == req.session.user_otp) {
+    req.session.login_id = req.session.user_id;
+    req.session.login_name = req.session.name;
+    res.redirect("/");
+  } else {
+    res.redirect("/accept_otp");
+  }
+});
 
 //create account
-router.get('/signup',(req,res)=>{
-    var obj={data:{
-        username:"",
-        useremail:"",
-        userpass:"",
-        userCpass:"",
-        err:""
-    }};
-    res.render('user/signup.ejs',obj);
-})
+router.get("/signup", (req, res) => {
+  var obj = {
+    data: {
+      username: "",
+      useremail: "",
+      userpass: "",
+      userCpass: "",
+      err: "",
+    },
+  };
+  res.render("user/signup.ejs", obj);
+});
 
-router.post('/createAccount',async(req,res)=>{
-   
-    var obj={data:req.body};
-   
-     var {username,err,useremail,userpass,userCpass}=req.body;
-     
-     var regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-     if(userpass!=userCpass){
-        req.body.err="Password is Not same!";
-        res.render('user/signup.ejs',obj)
-     }
-     else{
-      if(!regex.test(userpass))
-      {
-        req.body.err="Please Enter Strong Password!";
-        res.render('user/signup.ejs',obj)
-      }
-      else{
+router.post("/createAccount", async (req, res) => {
+  var obj = { data: req.body };
 
-         var sql=`insert into user(username,useremail,userpass) values(?,?,?)`;
-         var values=[username,useremail,userpass];
-         await exe(sql,values);
-         res.redirect('/sign');
-      }
-     }
-})
+  var { username, err, useremail, userpass, userCpass } = req.body;
 
+  var regex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  if (userpass != userCpass) {
+    req.body.err = "Password is Not same!";
+    res.render("user/signup.ejs", obj);
+  } else {
+    if (!regex.test(userpass)) {
+      req.body.err = "Please Enter Strong Password!";
+      res.render("user/signup.ejs", obj);
+    } else {
+      var sql = `insert into user(username,useremail,userpass) values(?,?,?)`;
+      var values = [username, useremail, userpass];
+      await exe(sql, values);
+      res.redirect("/sign");
+    }
+  }
+});
 
-
-module.exports=router;
+module.exports = router;
